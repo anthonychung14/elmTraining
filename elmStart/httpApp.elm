8d26f55c7fe36a7b8b04a5526fc9581fbbceac3b
Anthony Chung <anthonychung14@gmail.com>
2016-08-28 00:53:39 -0700

    HTTP requests work

--- elmStart/httpApp.elm
+++ elmStart/httpApp.elm
@@ -7,66 +7,84 @@ import Html.App as App
 
 import Styles
 import Http
+
+-- Wait what does the (:=) end up doing?
 import Json.Decode as Decode exposing ((:=))
 import Task
 
---TODO: FIGURE OUT BETTER DEFAULTS THAN "NONE"
 --MODELS
-type alias Problem = 
-  { time: Int 
-  , dataStruct: String 
-  }
-
+--Describes the shape of a subItem
 type alias Folder = 
   { name: String
   , html_url: String
   }
 
-init : (Folder, Cmd Msg)
+--Describes the shape of the whole state tree
+type alias Model =
+  { message: String
+  , folders: List Folder
+  }
+
+--TODO: FIGURE OUT BETTER DEFAULTS THAN "NONE"
+--After choosing, this will start the timer and display the random problem
+type alias Problem = 
+  { time: Int 
+  , dataStruct: String 
+  }
+
+init: (Model, Cmd Msg)
 init = 
-  (
-    { name = "Stuff"
-    , html_url = "None"
-    },
-    Cmd.none
-  )
+  let
+    model =
+    { message = "Hello there"
+    , folders = []
+    }
+  in
+    (model, Cmd.none)
   
 
 --VIEWS
-view : Folder -> Html Msg
+view : Model -> Html Msg
 view model =
-  div [ Styles.mainStyle ]
-  [ h1 [] [text "HTTP Requests"]
-  , div [] [
-    button [onClick GitStart] [text "GET from Github"]]
-  ]
+  let
+    showFolder folder =
+      li []
+        [ text folder.name ]
+  in
+    div [ Styles.mainStyle ]
+    [ h1 [] [text "HTTP Requests"]
+    , h2 [] [text model.message]
+    , div [] [      
+      button [onClick GitStart] [text "GET from Github"]]
+    , ul [] (List.map showFolder model.folders)
+    ]
 
 --UPDATE
 type Msg =
   NoOp  
   | GitStart
-  | FetchSucceed String
+  | FetchSucceed (List Folder)
   | FetchFail Http.Error
 
-update: Msg -> Folder -> (Folder, Cmd Msg)
+update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    
+  case msg of    
     NoOp ->
-      (model, Cmd.none)
+      model ! []
     
     GitStart ->
-      (model, getFolders fetchUrl)
+      ({ model | message = "INITIALIZE" }, fetchAll)
 
-    FetchFail _ ->
-      (model, Cmd.none)
+    FetchFail err ->
+      ({ model | message = "ERROR IN FETCH" ++ (toString err) }, Cmd.none)
     
-    FetchSucceed newUrl ->
-      (Folder model.name newUrl, Cmd.none)
+    FetchSucceed folders ->
+      ({ model | folders = folders, message = "FETCHED DATA" }, Cmd.none)
     
     
 fetchUrl: String
-fetchUrl = "https://api.github.com/repos/anthonychung14/gitajob/contents"
+fetchUrl = "https://api.github.com/repos/anthonychung14/algorithms/contents"
+--fetchUrl = "https://api.github.com/repos/anthonychung14/gitajob/contents"
 
 fetchAll: Cmd Msg
 fetchAll =
@@ -74,20 +92,18 @@ fetchAll =
     |> Task.perform FetchFail FetchSucceed
 
 decodeFolders: Decode.Decoder (List Folder)
-decodeFolders =
-  Decode.at ["data"] 
+decodeFolders =    
   Decode.list folderDecoder
 
 folderDecoder: Decode.Decoder Folder
 folderDecoder =
-  Decode.object3 Folder
+  Decode.object2 Folder
     ("name" := Decode.string)  
     ("html_url" := Decode.string)  
-
-
+    
 --HTTP REQS
-getFolders fetchUrl =
-  Task.perform FetchFail FetchSucceed (Http.get decodeFolders fetchUrl)
+--getFolders fetchUrl =
+--  Task.perform FetchFail FetchSucceed (Http.get decodeFolders fetchUrl)
 
 --MAIN
 main: Program Never
@@ -96,10 +112,5 @@ main =
       { init = init
       , view = view
       , update = update 
-      , subscriptions = subscriptions
+      , subscriptions = \_ -> Sub.none
       }
-
--- SUBSCRIPTIONS
-subscriptions : Folder -> Sub Msg
-subscriptions model =
-    Sub.none
